0249305a7d7c8898503c6b13deacb2bbd275147a
dytish <dashamihnik21@gmail.com>
2023-09-10 11:02:53 +0300

    ignor

--- pkg/config/config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '5097177562:AAF9HSpDw_TZsn_RkeflrkcV0BQ_12tciN0'
-
-CONFIG_BD = {
-    "user": "root",
-    "password": "12345",
-    "database": "ProWorkers",
-    "host": "185.195.24.47",
-    "port": "6666"
-}
